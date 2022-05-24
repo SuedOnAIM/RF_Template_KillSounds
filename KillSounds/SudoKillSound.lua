2d7df90b146ca78ada086a71342f51eae306e1ad
Namiya Tsudou <splendont@gmail.com>
2022-05-24 14:17:15 -0500

    Add files via upload

---
+++ KillSounds/SudoKillSound.lua
@@ -0,0 +1,21 @@
+-- Register the behaviour
+behaviour("SudoKillSound")
+
+function SudoKillSound:Start()
+    self.volume = self.script.mutator.GetConfigurationRange("KillSoundVolume")
+    local vol = self.volume/100;
+	self.targets.AudioSource.volume = vol
+    self.targets.AudioSource.SetOutputAudioMixer(AudioMixer.Important)
+    GameEvents.onActorDied.AddListener(self,"onActorDied")
+end
+
+function SudoKillSound:onActorDied(actor,source,isSilent)
+    if not actor.isPlayer and source == Player.actor and actor.team ~= Player.actor.team then
+        if self.script.mutator.GetConfigurationBool("RandomizeKillSound") then
+            self.targets.KillSoundBank.PlayRandom()
+        elseif not self.script.mutator.GetConfigurationBool("RandomizeKillSound") then
+            local index = self.script.mutator.GetConfigurationDropdown('ListKillSound')
+            self.targets.KillSoundBank.PlaySoundBank(index)
+        end
+    end
+end
